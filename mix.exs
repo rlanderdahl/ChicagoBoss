24a74b1c1b3f10544eb9e8fa771ffa7d8c2e3025
rlander <rlanderdahl@gmail.com>
2020-07-17 19:34:11 -0300

    chore: Update mixfile

--- mix.exs
+++ mix.exs
@@ -2,20 +2,35 @@ defmodule Boss.Mixfile do
   use Mix.Project
 
   def project do
-    [ app: :boss,
+    [
+      app: :boss,
       version: "0.9.0-beta2",
-      deps: deps(Mix.env),
+      elixir: "~> 1.9",
       name: "Chicago Boss",
-      source_url: "https://github.com/ChicagoBoss/ChicagoBoss",
-      elixir: "~> 0.10.3" ]
+      compilers: [:erlang, :app],
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
   end
 
+  # Run "mix help compile.app" to learn about applications.
   def application do
-    []
+    [
+      extra_applications: [:logger]
+    ]
   end
 
-  defp deps(_) do
-    [ { :ecto, github: "elixir-lang/ecto" },
-    { :postgrex, github: "ericmj/postgrex" } ]
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:boss_db, github: "rlanderdahl/boss_db", ref: "666f42c"},
+      {:tinymq, github: "ChicagoBoss/tinymq", tag: "v0.9.0"},
+      {:erlydtl, github: "erlydtl/erlydtl", ref: "118c176"},
+      {:iso8601, github: "danikp/erlang_iso8601", ref: "ae6a052017"},
+      {:mimetypes, github: "spawngrid/mimetypes", branch: "master"},
+      {:mochiweb, github: "mochi/mochiweb", ref: "53a9607"},
+      {:cowboy, github: "ninenines/cowboy", tag: "2.6.1"},
+      {:simple_bridge, github: "nitrogen/simple_bridge", ref: "1938ec7"}
+    ]
   end
 end
